4522d9b1e6c4256bf3ce1c9fcbf834c58d0b16ab
kboygit <heybisayangdako@gmail.com>
2016-12-28 15:33:31 -0800

    configured env,added gem nifty.. ,modified @books

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'nifty-generators'
 gem 'will_paginate'
 gem 'forgery'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize! do
+  config.gem 'nifty-generators'
   config.gem 'forgery'
   config.gem 'will_paginate'
 end
