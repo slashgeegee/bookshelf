ecde95271c5fb7f24166d4cce3711715c37a9d2a
kboygit <heybisayangdako@gmail.com>
2016-12-27 20:14:48 -0800

    generate controller Books

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # map.resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# ActionController::Routing::Routes.draw do |map|
+#   map.resources :books
+# end
